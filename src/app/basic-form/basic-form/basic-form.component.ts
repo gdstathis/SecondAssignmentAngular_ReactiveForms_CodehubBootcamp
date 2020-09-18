d01c9f6d0b3ed052036614b793584b9a6d08bddc
George Stathis <georgestathis1996@yahoo.gr>
2020-09-18 16:39:38 +0300

    Second Assignment in Angular -Forms

--- src/app/basic-form/basic-form/basic-form.component.ts
+++ src/app/basic-form/basic-form/basic-form.component.ts
@@ -1,23 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-basic-form',
-  templateUrl: './basic-form.component.html',
-  styleUrls: ['./basic-form.component.scss']
+  selector: "app-basic-form",
+  templateUrl: "./basic-form.component.html",
+  styleUrls: ["./basic-form.component.scss"],
 })
 export class BasicFormComponent implements OnInit {
   form: FormGroup;
-  programmingLanguages = ['TS', 'JS', 'C#'];
+  programmingLanguages = ["TS", "JS", "C#"];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.form = new FormGroup({
-      firstName: new FormControl(null, Validators.required),
-      lastName: new FormControl(null, Validators.required),
+      firstName: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      lastName: new FormControl(null, [
+        Validators.required,Validators.minLength(3)]),
+      isExperienced: new FormControl(null, Validators.required),
+      angular: new FormControl(null, Validators.required),
+      favouriteLanguage: new FormControl(null),
+      jsversion: new FormControl(null, Validators.required),
     });
 
+    this.form.get("favouriteLanguage").valueChanges.subscribe((value) => {
+      const jsVersionControl = this.form.get("jsversion");
+      if (value === "JS") {
+        jsVersionControl.setValidators(Validators.required);
+      } else {
+        jsVersionControl.clearValidators();
+      }
+      jsVersionControl.updateValueAndValidity();
+    });
   }
-
 }
